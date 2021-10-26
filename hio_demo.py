70f4283e19da2500d997bfb1b91b0d953963612c
Krister Ulvog <Krister@Kristers-MacBook-Pro.local>
2021-10-26 11:02:03 +0000

    added command args

--- hio_demo.py
+++ hio_demo.py
@@ -9,7 +9,10 @@ import tools
 parser = argparse.ArgumentParser()
 parser.add_argument('--image', type=str, required=True)
 parser.add_argument('--iter', type=int, default=2000)
+parser.add_argument('--beta', type=float, default=0.9)
 parser.add_argument('--noiselvl', type=float, default=0.)
+parser.add_argument('--display', action='store_true')
+parser.add_argument('--save', type=str, default=None)
 args = parser.parse_args()
 
 img = Image.open(args.image).convert('L')
@@ -28,11 +31,18 @@ mask[pad_len_1:-pad_len_1, pad_len_2:-pad_len_2] = True
 # noise = 0
 y = np.abs(tools.fft2d(img)) + np.random.normal(size=img.shape) * 1 / 255.
 
-x = algo.hio(y, mask, args.iter)
+x = algo.hio(y, mask, args.iter, beta=args.beta)
 # x2 = algo.oss(y, mask)
 
 img = img[mask].reshape((n, m))
 x = x[mask].reshape((n, m))
 # x2 = x2[mask].reshape((n, m))
 
-tools.stackview([img, x], method='Pillow')
+
+if args.display:
+    tools.stackview([img, x], method='Pillow')
+
+if args.save != None:
+    res = np.clip(v3 * 255, 0, 255)
+    res = Image.fromarray(res.astype(np.uint8))
+    res.save(args.save, format='PNG')

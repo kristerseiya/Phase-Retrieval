4821d400b1a783b98bf52c9004e7defafedeeb29
Krister Ulvog <Krister@Kristers-MacBook-Pro.local>
2021-10-26 12:00:20 +0000

    added noise level issue fixed

--- hio_demo.py
+++ hio_demo.py
@@ -11,7 +11,7 @@ parser.add_argument('--image', type=str, required=True)
 parser.add_argument('--samprate', type=float, default=2)
 parser.add_argument('--iter', type=int, default=2000)
 parser.add_argument('--beta', type=float, default=0.9)
-parser.add_argument('--noiselvl', type=float, default=0.)
+parser.add_argument('--noise', type=float, default=0.)
 parser.add_argument('--display', action='store_true')
 parser.add_argument('--save', type=str, default=None)
 args = parser.parse_args()
@@ -30,7 +30,7 @@ mask[pad_len_1:-pad_len_1, pad_len_2:-pad_len_2] = True
 # sd = 0.1 * np.sqrt(n*m)
 # noise = np.random.normal(size=img.shape) * sd + 1j * np.random.normal(size=img.shape) * sd
 # noise = 0
-y = np.abs(tools.fft2d(img)) + np.random.normal(size=img.shape) * 1 / 255.
+y = np.abs(tools.fft2d(img)) + np.random.normal(size=img.shape) * args.noise / 255.
 
 x = algo.hio(y, mask, args.iter, beta=args.beta)
 # x2 = algo.oss(y, mask)
